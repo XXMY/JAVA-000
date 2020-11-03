0b78ca22ed93928997c9ae174a5e67526a24f736
xxmy <qaz.wsx75369@163.com>
2020-11-03 20:09:06 +0800

    第三周作业3提交，代码：HomeworkHttpOutboundHandler.java,AddHeaderFilter.java

---
+++ Week_03/homework_3/nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundInitializer.java
@@ -0,0 +1,39 @@
+package io.github.kimmking.gateway.inbound;
+
+import io.github.kimmking.gateway.filter.AddHeaderFilter;
+import io.github.kimmking.gateway.filter.HttpRequestFilter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpInboundInitializer extends ChannelInitializer<SocketChannel> {
+	
+	private String proxyServer;
+	
+	public HttpInboundInitializer(String proxyServer) {
+		this.proxyServer = proxyServer;
+	}
+	
+	@Override
+	public void initChannel(SocketChannel ch) {
+		ChannelPipeline p = ch.pipeline();
+//		if (sslCtx != null) {
+//			p.addLast(sslCtx.newHandler(ch.alloc()));
+//		}
+		p.addLast(new HttpServerCodec());
+		//p.addLast(new HttpServerExpectContinueHandler());
+		p.addLast(new HttpObjectAggregator(1024 * 1024));
+		p.addLast(new HttpInboundHandler(this.proxyServer,filters()));
+	}
+
+	private List<HttpRequestFilter> filters(){
+		List<HttpRequestFilter> filters = new ArrayList<>();
+		filters.add(new AddHeaderFilter());
+		return filters;
+	}
+}
